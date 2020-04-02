a568e67cbb6fc25d581a71eb95ce0d00352fdedc
YaizaCano <yaiza.cano@est.fib.upc.edu>
2020-04-03 00:01:21 +0200

    histcounts vs histogram

--- chiSquare.m
+++ chiSquare.m
@@ -1,6 +1,6 @@
 function [sumred, sumblue] = chiSquare(model_red, model_blue, red, blue, nbins, sumred, sumblue)
 for i = 1:nbins 
-	sumred = sumred + ((model_red.Values(i) - red.Values(i))^2)/(model_red.Values(i)+red.Values(i)); 
-	sumblue = sumblue + ((model_blue.Values(i) - blue.Values(i))^2)/(model_blue.Values(i)+blue.Values(i)); 
+	sumred = sumred + ((model_red(i) - red(i))^2)/(model_red(i)+red(i)); 
+	sumblue = sumblue + ((model_blue(i) - blue(i))^2)/(model_blue(i)+blue(i)); 
 end
 end
--- computeQuadrants.m
+++ computeQuadrants.m
@@ -1,20 +1,20 @@
 function [maxred, maxblue] = computeQuadrants(model_red, model_blue, im, nbins, index, maxred, maxblue)
-	if index < 3
+	if index < 4
 		[q0,q1,q2,q3] = divideix(im);
         
-        [red, blue] = histRB(q0, nbins);
+        [red, blue] = histcountRB(q0, nbins);
         [sumred, sumblue] = chiSquare(model_red, model_blue, red, blue, nbins, 0, 0);
         [maxred, maxblue] = setMinimum(maxred, maxblue, sumred, sumblue);
         
-        [red, blue] = histRB(q1, nbins);
+        [red, blue] = histcountRB(q1, nbins);
         [sumred, sumblue] = chiSquare(model_red, model_blue, red, blue, nbins, 0, 0);
         [maxred, maxblue] = setMinimum(maxred, maxblue, sumred, sumblue);
         
-        [red, blue] = histRB(q2, nbins);
+        [red, blue] = histcountRB(q2, nbins);
         [sumred, sumblue] = chiSquare(model_red, model_blue, red, blue, nbins, 0, 0);
         [maxred, maxblue] = setMinimum(maxred, maxblue, sumred, sumblue);
         
-        [red, blue] = histRB(q3, nbins);
+        [red, blue] = histcountRB(q3, nbins);
         [sumred, sumblue] = chiSquare(model_red, model_blue, red, blue, nbins, 0, 0);
         [maxred, maxblue] = setMinimum(maxred, maxblue, sumred, sumblue);
         
