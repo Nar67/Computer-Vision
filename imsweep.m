e4ef06ecc7770ac9949687e64b0fc7860821ecdc
Nars <narcis.tg@gmail.com>
2020-04-05 14:13:28 +0200

    docs+

--- imsweep.m
+++ imsweep.m
@@ -6,8 +6,7 @@ function [chi] = imsweep(pattern_hist, im, nbins, sweep_rows, sweep_cols)
 chi = 20;
 
 [im_rows, im_cols, ~] = size(im);
-im = im2double(im);
-img = rgb2gray(im);
+img = im2double(im);
 for row = floor(sweep_rows/2) +1:(im_rows-floor(sweep_rows/2))
     for col = floor(sweep_cols/2) +1:(im_cols-floor(sweep_cols/2))
 		subim = img(row-floor(sweep_rows/2):row+floor(sweep_rows/2), col-floor(sweep_cols/2):col+floor(sweep_cols/2), :);
--- imsweepHSV.m
+++ imsweepHSV.m
@@ -15,14 +15,14 @@ img = normalizeHSV(img);
 for row = floor(sweep_rows/2) +1:(im_rows-floor(sweep_rows/2))
     for col = floor(sweep_cols/2) +1:(im_cols-floor(sweep_cols/2))
 		subim = img(row-floor(sweep_rows/2):row+floor(sweep_rows/2), col-floor(sweep_cols/2):col+floor(sweep_cols/2), :);
-        [h, v] = histcountsHS(subim, nbins)./numel(subim);
+        [h, v] = histcountHS(subim, nbins);
         chi_tmph = sum(((pattern_h - h).^2) ./ (pattern_h + h));
         chi_tmps = sum(((pattern_s - v).^2) ./ (pattern_s + v));
         if chi_tmph < chih && chi_tmps < chis
         	chih = chi_tmph;
         	chis = chi_tmps;
         end
-	end
+    end
 end
 chi = (chih + chis) /2;
 end
