57c753b48e919a34d9408325dda82d104c88698f
Nars <narcis.tg@gmail.com>
2020-04-05 14:35:26 +0200

    deleted shit

--- rmseRB.m
+++
@@ -1,8 +0,0 @@
-function [rmse] = rmseRB(histModel_red, histModel_blue, imhist_red, imhist_blue)
-%RMSERB Returns root mean squared error of two RB histograms
-
-rmse_red = sqrt(mean((histModel_red - imhist_red).^2));
-rmse_blue = sqrt(mean((histModel_blue - imhist_blue).^2));
-rmse = (rmse_red + rmse_blue) /2;
-
-end
